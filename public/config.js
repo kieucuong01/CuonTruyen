74065be978b1041dd6b64935808dffca8bfcb9cf
Cuong <kieucuong01@gmail.com>
2026-06-11 23:47:06 +0700

    Update generated public snapshots

--- public/config.js
+++ public/config.js
@@ -1,5 +1,11 @@
 window.COMIC_READER_CONFIG = {
   ...(window.COMIC_READER_CONFIG || {}),
-  "apiBaseUrl": "",
-  "importsBaseUrl": "https://s3.vn-hcm-1.vietnix.cloud/cuontruyen"
+  "apiBaseUrl": "https://cuontruyen.vercel.app",
+  "publicSnapshotBaseUrl": "/static-api",
+  "preferPublicSnapshots": true,
+  "importsBaseUrl": "https://s3.vn-hcm-1.vietnix.cloud/cuontruyen",
+  "monetization": {"adsEnabled":true,"donateUrl":"","adsProvider":"","adsenseClient":"","adsenseSlots":{"home":"","series":"","chapterEnd":""},"adsenseTestMode":false},
+  "publicSiteUrl": "https://cuontruyen.vercel.app",
+  "productionBaseUrl": "https://cuontruyen.vercel.app",
+  "enableLocalCrawlerUi": false
 };
